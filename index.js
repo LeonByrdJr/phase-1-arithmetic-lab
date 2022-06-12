5d893cc0be42ce08d7097623ad6debfd20f42418
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2022-06-12 18:47:58 -0500

    commit tree 68f84afc1068a27afc45477ad4b652d963fd14a6

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 6.2
+const num2 = 10
+const num3 = 16
+const num4 = 6
+const numberSet = (1, 4, 13, 15, 20)
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()) + 1 ;
+
+const mod = num3 % num4
+
+const max = Math.max(numberSet)
